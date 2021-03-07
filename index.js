5b10a676cd94dea2069e42a16c172ebe6b79d60d
Kunal <kdshah6593@gmail.com>
2021-03-07 16:28:18 -0500

    move deck form handling to Deck class

--- index.js
+++ index.js
@@ -2,57 +2,15 @@ const endPoint = "http://localhost:3000/api/v1/decks";
 const decksList = document.getElementById('decks-list')
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
     Deck.getDecks()
 
     const newDeckBtn = document.getElementById('new-deck-btn')
-    newDeckBtn.addEventListener('click', handleNewDeckDisplay)
+    newDeckBtn.addEventListener('click', Deck.handleNewDeckDisplay)
 
     const newDeckForm = document.getElementById('new-deck-form')
-    newDeckForm.addEventListener('submit', (e) => handleNewDeckSubmit(e))
+    newDeckForm.addEventListener('submit', (e) => Deck.handleNewDeckSubmit(e))
 })
-
-
-const handleNewDeckDisplay = () => {
-    const newDeckForm = document.getElementById('new-deck')
-    if (newDeckForm.style.display === 'none') {
-        newDeckForm.style.display = ''
-    } else {
-        newDeckForm.style.display = 'none'
-    }
-}
-
-const handleNewDeckSubmit = (e) => {
-    e.preventDefault()
-    const inputTitle = document.querySelector('#input-title').value
-    const inputUserId = parseInt("1") //need to remove the hard-coded user
-
-    postNewDeck(inputTitle, inputUserId)
-    // reset form
-    const deckForm = document.getElementById('new-deck-form')
-    deckForm.reset()
-    // hide form
-    handleNewDeckDisplay()
-}
-
-const postNewDeck = (title, user_id) => {
-    const inputData = {title, user_id}
-
-    fetch(endPoint, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(inputData)
-    })
-    .then(resp => resp.json())
-    .then(deck => {
-        const newDeck = new Deck(deck.data, deck.data.attributes)
-        newDeck.renderDeck()
-    })
-}
 
 //fetch http://localhost:3000/api/v1/decks
 //fetch http://localhost:3000/api/v1/users/1
--- src/deck.js
+++ src/deck.js
@@ -52,4 +52,44 @@ class Deck {
     }
 
 
+    static handleNewDeckDisplay = () => {
+        const newDeckForm = document.getElementById('new-deck')
+        if (newDeckForm.style.display === 'none') {
+            newDeckForm.style.display = ''
+        } else {
+            newDeckForm.style.display = 'none'
+        }
+    }
+
+    static handleNewDeckSubmit = (e) => {
+        e.preventDefault()
+        const inputTitle = document.querySelector('#input-title').value
+        const inputUserId = parseInt("1") //need to remove the hard-coded user
+    
+        this.postNewDeck(inputTitle, inputUserId)
+        // reset form
+        const deckForm = document.getElementById('new-deck-form')
+        deckForm.reset()
+        // hide form
+        this.handleNewDeckDisplay()
+    }
+    
+    static postNewDeck = (title, user_id) => {
+        const inputData = {title, user_id}
+    
+        fetch(endPoint, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(inputData)
+        })
+        .then(resp => resp.json())
+        .then(deck => {
+            const newDeck = new Deck(deck.data, deck.data.attributes)
+            newDeck.renderDeck()
+        })
+    }
+
 }
